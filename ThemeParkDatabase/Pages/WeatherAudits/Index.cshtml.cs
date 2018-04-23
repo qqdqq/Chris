4be4616f1ce2a1726a4a679255074a83b2d46054
TheZebull <nicholas.ghansyam@gmail.com>
2018-04-23 16:56:44 -0500

    commit

--- ThemeParkDatabase/Pages/WeatherAudits/Index.cshtml.cs
+++ ThemeParkDatabase/Pages/WeatherAudits/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,8 @@ using ThemeParkDatabase.Models;
 
 namespace ThemeParkDatabase.Pages.WeatherAudits
 {
+    [Authorize(Roles = "Admin,Manager")]
+
     public class IndexModel : PageModel
     {
         private readonly ThemeParkDatabase.Models.ThemeParkDatabaseContext _context;
